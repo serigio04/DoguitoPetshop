43d208a0aba112c8212f3f0802a566847552facb
Sergio gomar <sergio.gomar04@gmail.com>
2024-01-13 17:20:38 -0600

    Terminando DOM de pets e inicializando servidor para solicitud POST

--- controllers/agregarPet.js
+++ controllers/agregarPet.js
@@ -1,5 +1,46 @@
 (() => {
-    const name = document.getElementById("name");
-    const edad = document.getElementById("age");
-    const img  = document.getElementById("img")
-})
+    
+    const btn = document.querySelector('[data-button]')
+
+    const añadirMascota = (e) => {
+        e.preventDefault();
+        const name = document.getElementById("name");
+        const edad = document.getElementById("age");
+        const img  = document.getElementById("img");
+        const div_m = document.createElement("div");
+        div_m.classList.add("mascota");
+        const div_mImg = document.createElement("div");
+        div_mImg.classList.add("mascota_img");
+        const pet_img = document.createElement("img");
+        pet_img.classList.add("mascota_img-pic");
+        div_mImg.src = img;
+        div_mImg.appendChild(pet_img); //
+        const div_mInfo = document.createElement("div");
+        div_mInfo.classList.add("mascota_info");
+        const h4 = document.createElement("h4");
+        const h6 = document.createElement("h6");
+        h4.textContent = name;
+        h6.textContent = edad;
+        div_mInfo.appendChild(h4);
+        div_mInfo.appendChild(h6);
+        div_m.appendChild(div_mImg);
+        div_m.appendChild(div_mInfo);
+
+        fetch('http://localhost:5500/agregarMascota', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, edad, img }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.mensaje);
+        })
+        .catch(error => {
+            console.error('Error al agregar la mascota:', error);
+        });
+    }
+
+    btn.addEventListener('click', añadirMascota)
+})();
---
+++ service/service.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+app.use(cors());
+app.use(express.static('public')); // Sirve archivos estáticos desde la carpeta 'public'
+
+app.use(express.json());
+
+app.post('/agregarMascota', (req, res) => {
+  const { name, edad, img } = req.body;
+
+  // Crear objeto de mascota
+  const mascota = {
+    name,
+    edad,
+    img,
+  };
+
+  // Convertir el objeto a JSON
+  const mascotaJSON = JSON.stringify(mascota);
+
+  // Ruta donde se almacenará el archivo
+  const rutaArchivo = path.join(__dirname, 'public', 'mascotas.json');
+
+  // Escribir en el archivo
+  fs.appendFileSync(rutaArchivo, `${mascotaJSON}\n`);
+
+  res.json({ mensaje: 'Mascota agregada exitosamente' });
+});
+
+const puerto = 5500;
+app.listen(puerto, () => {
+  console.log(`Servidor escuchando en http://localhost:${puerto}`);
+});
